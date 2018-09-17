10dc8bd9d13388e07e7f39184eb3a0604a79aaea
Alex Childers <jamesalexchilders@gmail.com>
2018-09-16 22:33:45 -0500

    Fixed multiple nested comments

--- src/commentMapper.js
+++ src/commentMapper.js
@@ -1,13 +1,20 @@
 import Comment from "./models/comment";
 
 export const generateComments = json => {
-    return json.data.children.map(generateComment);
+    const comments =  json.data.children.map(generateComment);
+    return comments[0]; //Need to remove this extra array wrapping in a pretty way
 };
 
 export const generateComment = json => {
+    const comments = [];
     if (!json.data.body) return;
     if (json.data.replies) {
-        json.data.replies.data.children.map(generateComment);
+        comments.push(new Comment(json.data));
+        json.data.replies.data.children.map(reply => {
+            const childComment = generateComment(reply);
+            comments.push(childComment);
+        });ß
+        return comments;
     }
     return new Comment(json.data);
 };
